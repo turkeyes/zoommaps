965c40dc9c99f60a9933059e1331620ba89fb7eb
Matthew Tancik <tancik@Matthews-MacBook-Pro.local>
2017-08-18 12:06:53 -0400

    save to db in chunks

--- models/label.js
+++ models/label.js
@@ -2,11 +2,14 @@ var mongoose = require("mongoose");
 
 var labelSchema = mongoose.Schema({
   src: String,
-	x_min: Number,
-  x_max: Number,
-  y_min: Number,
-  y_max: Number,
-  id: String,
+  data: [{
+    x_min: Number,
+    x_max: Number,
+    y_min: Number,
+    y_max: Number,
+    time: Number
+  }],
+  id: String
 });
 
 var Label = mongoose.model("Label", labelSchema);
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-var pswpElement = document.querySelectorAll('.pswp')[0];
+var min_events = 10
 
+var pswpElement = document.querySelectorAll('.pswp')[0];
 var uniq = 'id' + (new Date()).getTime();
 // build items array
 var items = [
@@ -57,28 +58,39 @@ var options = {
 var pswp = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
 pswp.init();
 
-pswp.listen('position_change', function(item, x, y, zoom, time) {
-  x_min = x < 0 ? Math.floor(-x) : 0;
-  width = Math.floor(-x + screen.width / zoom)
-  x_max = width < item.w ? width : item.w
+var src = '';
+var data = [];
 
-  y_min = y < 0 ? Math.floor(-y) : 0;
-  height = Math.floor(-y + screen.height / zoom)
-  y_max = height < item.h ? height : item.h
-  console.log('img.src:' + item.src + ' x_min:' + x_min + ' x_max:' + x_max + ' y_min:' + y_min + ' y_max:' + y_max + ' time:' + time);
-  $.ajax({
-        type: "POST",
-        url: "/data",
-        data: JSON.stringify({ src:item.src, x_min:x_min, x_max:x_max, y_min:y_min, y_max:y_max, id:uniq}),
-        contentType: "application/json",
-        success: function(res) {
-            if (res.success) {
-            } else {
-                console.log("failed message");
+pswp.listen('position_change', function(item, x, y, zoom, time) {
+  if (item.src !== src) {
+    if (data.length > min_events) {
+      console.log("saving to db")
+      $.ajax({
+            type: "POST",
+            url: "/data",
+            data: JSON.stringify({ src:item.src, data:data, id:uniq}),
+            contentType: "application/json",
+            success: function(res) {
+                if (res.success) {
+                } else {
+                    console.log("failed message");
+                }
+            },
+            error: function(err) {
+                console.log("could not connect to db server");
             }
-        },
-        error: function(err) {
-            console.log("could not connect to db server");
-        }
-    });
+        });
+    }
+    src = item.src;
+    data = [];
+  }
+  var x_min = x < 0 ? Math.floor(-x) : 0;
+  var width = Math.floor(-x + screen.width / zoom);
+  var x_max = width < item.w ? width : item.w;
+
+  var y_min = y < 0 ? Math.floor(-y) : 0;
+  var height = Math.floor(-y + screen.height / zoom);
+  var y_max = height < item.h ? height : item.h;
+  console.log('img.src:' + item.src + ' x_min:' + x_min + ' x_max:' + x_max + ' y_min:' + y_min + ' y_max:' + y_max + ' time:' + time);
+  data.push({ x_min:x_min, x_max:x_max, y_min:y_min, y_max:y_max, time: time });
 });
