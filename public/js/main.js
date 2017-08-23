b24bf9fb50937a97ccc9626e90db50514a2cfa6e
Matthew Tancik <tancik@30-10-125.wireless.csail.mit.edu>
2017-08-23 15:22:53 -0400

    fix window values

--- public/js/main.js
+++ public/js/main.js
@@ -59,6 +59,16 @@ var options = {
     closeOnVerticalDrag: false
 };
 
+function getOrientation(){
+    var orientation = window.innerWidth > window.innerHeight ? "Landscape" : "Portrait";
+    return orientation;
+}
+
+window.onresize = function(){
+  orientation = getOrientation();
+  console.log(orientation)
+}
+
 // Initializes and opens PhotoSwipe
 var pswp = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
 pswp.init();
@@ -70,6 +80,12 @@ var y_min = [];
 var y_max = [];
 var times = [];
 var start_time = 0;
+var x_offset = 0;
+var y_offset = 0;
+
+pswp.listen('setImageSize', function(w, h) {
+  console.log('w:'+ w + ' h:' + h)
+})
 
 pswp.listen('position_change', function(item, x, y, zoom, time) {
   if (item.src !== src) {
@@ -98,15 +114,20 @@ pswp.listen('position_change', function(item, x, y, zoom, time) {
     y_max = [];
     times = [];
     start_time = time;
+    x_offset = x;
+    y_offset = y;
   }
-  x_min.push(x < 0 ? Math.floor(-x) : 0);
-  var width = Math.floor(-x + screen.width / zoom);
+  x_min.push(x < 0 ? Math.floor(-x / zoom) : 0);
+  var width = Math.floor(((-x+x_offset*2.0) + item.w*item.fitRatio) / zoom);
   x_max.push(width < item.w ? width : item.w);
 
-  y_min.push(y < 0 ? Math.floor(-y) : 0);
-  var height = Math.floor(-y + screen.height / zoom);
+  y_min.push(y < 0 ? Math.floor(-y / zoom) : 0);
+  var height = Math.floor(((-y+y_offset*2.0) + item.h*item.fitRatio) / zoom);
   y_max.push(height < item.h ? height : item.h);
   times.push(time - start_time)
-  console.log('img.src:' + item.src + ' x_min:' + x_min[x_min.length - 1] + ' x_max:' + x_max[x_max.length - 1] + ' y_min:' + y_min[y_min.length - 1] + ' y_max:' + y_max[y_max.length - 1] + ' time:' + time[time.length - 1]);
+  // console.log(item)
+  // console.log(item.fitRatio)
+  // console.log('x:' + -x/zoom + ' y:' + -y/zoom + ' zoom:' + zoom)
+  // console.log('img.src:' + item.src + ' x_min:' + x_min[x_min.length - 1] + ' x_max:' + x_max[x_max.length - 1] + ' y_min:' + y_min[y_min.length - 1] + ' y_max:' + y_max[y_max.length - 1] + ' time:' + time[time.length - 1]);
   //data.push({ x_min:x_min, x_max:x_max, y_min:y_min, y_max:y_max, time: time });
 });
