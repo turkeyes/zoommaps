c5db35d30c664ab823651ab53b662d969266cb0f
Anelise Newman <apnewman@mit.edu>
2017-12-11 18:41:47 -0500

    kimberli task

--- public/datasets/make_indiv_json.py
+++ public/datasets/make_indiv_json.py
@@ -7,28 +7,29 @@ with open('studies_12_2017_all.json', 'r') as infile:
 
 posters = data[0]["data"] #array of posters
 
-#subjects = []
-#k = 6
-#for subject in subjects: 
-#    posters_for_subject = random.choice(posters, k)
-#    task = [
-#            {
-#                "name": "poster viewing study",
-#                "data": posters
-#            }
-#    ]
-#    with open('studies_12_2017_%s.json' % subject, 'w') as outfile: 
-#        json.dump(task, outfile) 
-#
-#
+def make_subjects(): 
+    subjects = ['kimberli']
+    for subject in subjects: 
+        random.shuffle(posters)
+        task = [
+                {
+                    "name": "poster viewing study",
+                    "data": posters
+                }
+        ]
+        with open('studies_12_2017_%s.json' % subject, 'w') as outfile: 
+            json.dump(task, outfile) 
 
 
-for i, poster in enumerate(posters): 
-    singleton = [
-            {
-                "name": data[0]["name"],
-                "data": [poster]
-            }
-    ]
-    with open('studies_12_2017_%d.json' % i, 'w') as outfile: 
-        json.dump(singleton, outfile)
+def make_singletons(): 
+    for i, poster in enumerate(posters): 
+        singleton = [
+                {
+                    "name": data[0]["name"],
+                    "data": [poster]
+                }
+        ]
+        with open('studies_12_2017_%d.json' % i, 'w') as outfile: 
+            json.dump(singleton, outfile)
+
+make_subjects()
