5baaa8c73ef6cceeb177393e1533399872d7f584
Shan <slu5@wellesley.edu>
2018-11-19 16:46:18 -0500

    add mobile checking

--- routes/index.js
+++ routes/index.js
@@ -9,38 +9,35 @@ var Label = require("../models/label");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
+    var md = new MobileDetect(req.headers['user-agent']);
+    if (!Boolean(md.mobile() || md.phone() || md.tablet())) {
+      res.send({ success: false, message: "Not on mobile device!" });
+    }
     res.sendFile(path.join(__dirname, "../views/viewer.html"));
 });
 
 router.get("/:dataset/:tag", function(req, res, next) {
+  var md = new MobileDetect(req.headers['user-agent']);
+  if (!Boolean(md.mobile() || md.phone() || md.tablet())) {
+    res.send({ success: false, message: "Not on mobile device!" });
+  }
   res.redirect('/?dataset=' + req.params['dataset'] + '&tag=' + req.params['tag']);
 })
 
 router.get("/:dataset", function(req, res, next) {
   var md = new MobileDetect(req.headers['user-agent']);
-  console.log( md.mobile() );
-  console.log( md.phone() );
-  console.log( md.tablet() );
-  if !(md.mobile() || md.phone() || md.tablet()) {
-    res.status(403).send({ success: false, message: "Not on mobile device!" });
+  if (!Boolean(md.mobile() || md.phone() || md.tablet())) {
+    res.send({ success: false, message: "Not on mobile device!" });
   }
   res.redirect('/?dataset=' + req.params['dataset']);
 })
 
 // send data
 router.post("/data", function(req, res, next) {
-    var md = new MobileDetect(req.headers['user-agent']);
-    console.log( md.mobile() );
-    console.log( md.phone() );
-    console.log( md.tablet() );
-    if (md.mobile() || md.phone() || md.tablet()) {
-      var label = new Label({ src:req.body.src, x_min:req.body.x_min, x_max:req.body.x_max, y_min:req.body.y_min, y_max:req.body.y_max, time:req.body.time, id:req.body.id, tag:req.body.tag, dataset:req.body.dataset});
-      label.save(function(err) {
-          if (err) res.send({ success: false, message: "Can't save label, please try again!" });
-      });
-    } else {
-      res.status(403).send({ success: false, message: "Not on mobile device!" });
-    }
+  var label = new Label({ src:req.body.src, x_min:req.body.x_min, x_max:req.body.x_max, y_min:req.body.y_min, y_max:req.body.y_max, time:req.body.time, id:req.body.id, tag:req.body.tag, dataset:req.body.dataset});
+  label.save(function(err) {
+    if (err) res.send({ success: false, message: "Can't save label, please try again!" });
+  });
 });
 
 module.exports = router;
