7790c78a7c793ab2704206c6024f4eef8c71e5bb
Barry Anthony McNamara III <bamazap@hotmail.com>
2019-03-08 00:30:56 -0500

    Don't let the mobile link be too long

--- src/instructions/instructions.js
+++ src/instructions/instructions.js
@@ -8,13 +8,12 @@ import { humanRange } from '../utils';
 export default function main($container, data, onValidKey) {
   const $instructions = $(html);
   $instructions.find('.title').text(`Zoom Maps: ${data.bigName}`);
-
   const $overview = $instructions.find('.overview');
   $overview.html(overviewHTML);
 
+  // overview text
   const [minSecPhoto, maxSecPhoto] = humanRange(data.minSecPhoto, 5, 1.25);
   const [minMinTotal, maxMinTotal] = humanRange(data.minSecTotal / 60, 1, 1.5);
-
   $overview.find('.num-photos').text(15);
   $overview.find('.big-name').text(data.bigName);
   $overview.find('.small-name').text(data.smallName);
@@ -23,13 +22,21 @@ export default function main($container, data, onValidKey) {
   $overview.find('.min-min-total').text(minMinTotal);
   $overview.find('.max-min-total').text(maxMinTotal);
 
-  const mobileLink = window.location.origin + '/viewer' + window.location.search;
+  // qr code
+  const urlParams = new URLSearchParams(window.location.search);
+  let workerId = urlParams.get('workerId');
+  const dataset = urlParams.get('dataset');
+  const mobileLink = window.location.origin
+    + '/viewer'
+    + `?dataset=${dataset}&workerId=${workerId}`;
   const qr = qrcode(0, 'L');
   qr.addData(mobileLink);
   qr.make();
   $instructions.find('.qr .code')
     .append(qr.createSvgTag({ scalable: true, margin: 0 }));
   $instructions.find('.qr .link').text(mobileLink);
+
+  // on submit key
   $instructions.find('.enter-key').submit(async (e) => {
     e.preventDefault();
     const submitKey = $instructions.find('.enter-key input').val();
